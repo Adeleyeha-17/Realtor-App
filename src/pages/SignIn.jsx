61717d05f01d3a8e0666b169fd415740f34a7b0b
unknown <Adeleyeha.17@protonmail.com>
2023-05-17 13:46:47 +0100

    completed signin functionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,10 +1,14 @@
 
 import { useState } from 'react';
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate} from 'react-router-dom';
 import OAuth from '../components/OAuth.jsx'
+import { signInWithEmailAndPassword, getAuth } from 'firebase/auth';
+import { toast } from 'react-toastify';
 
 export default function SignIn() {
+  const navigate = useNavigate()
+
   const [formData, setFormData] = useState({
     email: "",
     password: "",
@@ -29,6 +33,24 @@ export default function SignIn() {
     }))
   }
 
+    async function onSubmit(e) {
+      e.preventDefault();
+
+      try {
+        const auth = getAuth()
+        const userCredentials = await signInWithEmailAndPassword(
+          auth, email, password
+        )
+
+        if(userCredentials.user){
+          navigate("/")
+        }
+
+      } catch (error) {
+        toast.error("Check your email and password again")
+      }
+    }
+
   return (
     <section>
       <h1 className="text-3xl text-center font-bold mt-6">Sign In</h1>
@@ -37,7 +59,7 @@ export default function SignIn() {
           <img src="https://images.unsplash.com/photo-1682347546932-f397ccf63940?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="beach" className="ml-6 rounded-2xl h-84 w-84" />
         </div>
         <div className="w-full md:w-[60%] lg:w-[40%] lg:ml-20 sm:w-[50%] items-center justify-center">
-          <form>
+          <form onSubmit={onSubmit}>
             <input className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out mb-4" type="text" name="email" value={email} placeholder="Email Address" onChange={onChange} />
             <div className="relative">
               <input className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out mb-4" type={showPassword ? "text" : "password"} name="password" value={password} placeholder="Password" onChange={onChange} />
