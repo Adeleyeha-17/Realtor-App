323600805466d368fcf424af3ae3f83963868914
unknown <Adeleyeha.17@protonmail.com>
2023-05-26 03:53:56 +0100

    created PrivateRoute component

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,12 @@
+import { getAuth } from "firebase/auth"
 import { useState } from "react"
 
 
 export default function Profile(){
-
+  const auth = getAuth()
   const [formData, setFormData] = useState({
-    fullname: "Adeleye Habeeb",
-    email: "hayden10beb@gmail.com"
+    fullname: auth.currentUser.displayName,
+    email: auth.currentUser.email
   })
  
   const {fullname, email} = formData
@@ -23,12 +24,19 @@ export default function Profile(){
     <div className="max-w-6xl mx-auto flex flex-col justify-center items-center">
       <section>
         <h1 className="text-3xl text-center font-bold mt-6 mb-3">Profile</h1>
-      <div className="sm:w-[60%] md:w-[70%] lg:w-[80%] mt-6 px-3 mx-auto">
+      <div className="sm:w-full md:w-[85%] mt-6 px-3 mx-auto">
       <form>
-          {/* Name input */} 
+          {/* Name input */}
+          <input type="text" name="fullname" value={fullname} onClick={onClickFunction} disabled className="w-full rounded text-xl text-gray-700 bg-white px-4 py-2 my-2 border border-gray-300 transition ease-in-out"/>
+          
+          {/* Email input */}
+          <input type="email" name="email" value={email} onClick={onClickFunction} disabled className="w-full rounded text-xl text-gray-700 bg-white px-4 py-2 my-2 border border-gray-300 transition ease-in-out"/>
 
-          <input type="text" name="fullname" value={fullname} onClick={onClickFunction} disabled className="w-full rounded text-xl text-gray-700 bg-white mx-4 my-2 border border-gray-300 transition ease-in-out"/>
-          <input type="email" name="email" value={email} onClick={onClickFunction} className="w-full rounded text-xl text-gray-700 bg-white mx-4 my-2 border border-gray-300 transition ease-in-out"/>
+          <div className="flex justify-between whitespace-nowrap text-sm mb-4 items-center">
+            <p className="flex">Do you want to change your name? <span className="ml-1 text-red-600 hover:text-red-700 transition ease-in-out duration-200 cursor-pointer">Edit</span></p>
+            <p className="text-blue-600 hover:text-blue-800 transition ease-in-out duration-200 cursor-pointer">Sign out</p>
+
+          </div>
         </form>
       </div>
         
