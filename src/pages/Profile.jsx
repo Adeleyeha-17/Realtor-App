9cc26c87dd68ba0a82d10714ccc3860196943ae2
unknown <Adeleyeha.17@protonmail.com>
2023-05-30 03:36:18 +0100

    completed the edit functionality, updating the batabase in firestore

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,14 @@
-import { getAuth } from "firebase/auth"
+import { getAuth, updateProfile } from "firebase/auth"
+import { doc, updateDoc } from "firebase/firestore"
 import { useState } from "react"
 import { useNavigate } from "react-router"
+import { toast } from "react-toastify"
+import { db } from "../firebase.config"
 
 
 export default function Profile(){
   const auth = getAuth()
+  const [changeDetails, setChangeDetails] = useState(false)
   const [formData, setFormData] = useState({
     fullname: auth.currentUser.displayName,
     email: auth.currentUser.email
@@ -13,11 +17,11 @@ export default function Profile(){
   const {fullname, email} = formData
   const navigate = useNavigate()
 
-  function onClickFunction(e){
+  function onChange(e){
     const {name, value} = e.target
     setFormData( prevState => ({
       ...prevState,
-      [name] : value
+      [name] : value,
     }))
   }
   
@@ -25,6 +29,37 @@ export default function Profile(){
     auth.signOut()
     navigate("/")
   }
+
+  function editDetails() {
+    {
+      changeDetails && onSubmit();
+      setChangeDetails( prevState => !prevState)
+    }
+    
+  }
+  
+
+ async function onSubmit() {
+try {
+  // Update user display name
+  if(auth.currentUser.displayName !== fullname){
+    await updateProfile(auth.currentUser, {
+      displayName: fullname
+    })
+  }
+
+  // update user display name in firestore
+
+  const docRef = doc(db, "users", auth.currentUser.uid)
+  await updateDoc(docRef, {
+    name: fullname,
+  })
+  toast.success("Changes made")
+} catch (error) {
+  toast.error("Could not update the profile data")
+}
+  }
+  
   return(
     <div className="max-w-6xl mx-auto flex flex-col justify-center items-center">
       <section>
@@ -32,13 +67,14 @@ export default function Profile(){
       <div className="sm:w-full md:w-[85%] mt-6 px-3 mx-auto">
       <form>
           {/* Name input */}
-          <input type="text" name="fullname" value={fullname} onClick={onClickFunction} disabled className="w-full rounded text-xl text-gray-700 bg-white px-4 py-2 my-2 border border-gray-300 transition ease-in-out"/>
+          <input type="text" name="fullname" value={fullname} onChange={onChange} disabled={!changeDetails} className={`w-full rounded text-xl text-gray-700 bg-white px-4 py-2 my-2 border border-gray-300 transition ease-in-out ${changeDetails && "bg-red-200 focus:bg-red-200"}`}/>
           
           {/* Email input */}
-          <input type="email" name="email" value={email} onClick={onClickFunction} disabled className="w-full rounded text-xl text-gray-700 bg-white px-4 py-2 my-2 border border-gray-300 transition ease-in-out"/>
+          <input type="email" name="email" value={email} onChange={onChange} disabled={!changeDetails} className={`w-full rounded text-xl text-gray-700 bg-white px-4 py-2 my-2 border border-gray-300 transition ease-in-out`}/>
 
           <div className="flex justify-between whitespace-nowrap text-sm mb-4 items-center">
-            <p className="flex">Do you want to change your name? <span className="ml-1 text-red-600 hover:text-red-700 transition ease-in-out duration-200 cursor-pointer">Edit</span></p>
+            <p className="flex">Do you want to change your name? <span className="ml-1 text-red-600 hover:text-red-700 transition ease-in-out duration-200 cursor-pointer" onClick={editDetails}>
+              {changeDetails ? "Apply change" : "Edit"}</span></p>
             <p className="text-blue-600 hover:text-blue-800 transition ease-in-out duration-200 cursor-pointer" onClick={onSignOut}>Sign out</p>
 
           </div>
